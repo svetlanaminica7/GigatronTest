fabc39efaf01f6cb0ebd368fd7a28c161926673e
QA1.6 <svetlanaminica7@gmail.com>
2023-07-13 18:39:24 +0200

    init

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    ChromeDriver driver;
+
+    public ChromeDriver openBrowser()
+    {
+        ChromeDriver driver1 = new ChromeDriver();
+        driver.navigate().to("https://gigatron.rs/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+
+    }
+}
